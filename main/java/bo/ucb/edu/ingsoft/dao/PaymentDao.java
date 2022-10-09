b519b3a0dcb7d9799b86b52b7ad7fc5634234d97
AracelyCondoriTipola <aracely96.ac>
2022-10-08 23:54:18 -0400

    modicando tablas

--- main/java/bo/ucb/edu/ingsoft/dao/PaymentDao.java
+++ main/java/bo/ucb/edu/ingsoft/dao/PaymentDao.java
@@ -3,3 +3,7 @@ package bo.ucb.edu.ingsoft.dao;
 import bo.ucb.edu.ingsoft.model.Payment;
 import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
+public interface PaymentDao {
+    public void createPayment(Payment payment);
+}
