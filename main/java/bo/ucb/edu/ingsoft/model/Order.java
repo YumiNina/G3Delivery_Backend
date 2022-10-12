53d379d17d168679fa1beab5d3269f517f0ca037
AracelyCondoriTipola <aracely96.ac>
2022-10-12 09:09:31 -0400

    corrigiendo datos bbdd

--- main/java/bo/ucb/edu/ingsoft/model/Order.java
+++ main/java/bo/ucb/edu/ingsoft/model/Order.java
@@ -5,5 +5,146 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Service
+public class Order {
+    private Integer orderId;
+    private Integer productId;
+    private Integer addressId;
+    private Integer userId;
+    private Date date;
+    private Integer status;
+    private Integer state;
+    private BigDecimal total;
+    private Integer txId;
+    private String txHost;
+    private Integer txUserId;
+    private Date txDate;
+    private Integer txStatus;
 
+    public Order(){
+
+    }
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Integer addressId) {
+        this.addressId = addressId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getState() {
+        return state;
+    }
+
+    public void setState(Integer state) {
+        this.state = state;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Integer getTxId() {
+        return txId;
+    }
+
+    public void setTxId(Integer txId) {
+        this.txId = txId;
+    }
+
+    public String getTxHost() {
+        return txHost;
+    }
+
+    public void setTxHost(String txHost) {
+        this.txHost = txHost;
+    }
+
+    public Integer getTxUserId() {
+        return txUserId;
+    }
+
+    public void setTxUserId(Integer txUserId) {
+        this.txUserId = txUserId;
+    }
+
+    public Date getTxDate() {
+        return txDate;
+    }
+
+    public void setTxDate(Date txDate) {
+        this.txDate = txDate;
+    }
+
+    public Integer getTxStatus() {
+        return txStatus;
+    }
+
+    public void setTxStatus(Integer txStatus) {
+        this.txStatus = txStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", productId=" + productId +
+                ", addressId=" + addressId +
+                ", userId=" + userId +
+                ", date=" + date +
+                ", status=" + status +
+                ", state=" + state +
+                ", total=" + total +
+                ", txId=" + txId +
+                ", txHost='" + txHost + '\'' +
+                ", txUserId=" + txUserId +
+                ", txDate=" + txDate +
+                ", txStatus=" + txStatus +
+                '}';
+    }
 }
