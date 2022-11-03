727d85d3f371e175458c33f2a8e0f293d91bfcdd
AracelyCondoriTipola <aracely96.ac>
2022-11-03 00:00:03 -0400

    dto agregando bbdd para administrador

--- src/main/java/bo/ucb/edu/ingsoft/dto/OrderState.java
+++ src/main/java/bo/ucb/edu/ingsoft/dto/OrderState.java
@@ -6,22 +6,5 @@ import org.springframework.stereotype.Service;
 public class OrderState {
     private Integer state;
 
-    public OrderState(){
-
-    }
-
-    public Integer getState() {
-        return state;
-    }
-
-    public void setState(Integer state) {
-        this.state = state;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderState{" +
-                "state=" + state +
-                '}';
-    }
+    
 }
