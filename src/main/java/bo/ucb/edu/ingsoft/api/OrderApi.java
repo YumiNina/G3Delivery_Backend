04b45cde9f793c2c71803ddba05635f14ef61ea2
AracelyCondoriTipola <aracely96.ac>
2022-11-02 23:55:41 -0400

    modifindano api

--- src/main/java/bo/ucb/edu/ingsoft/api/OrderApi.java
+++ src/main/java/bo/ucb/edu/ingsoft/api/OrderApi.java
@@ -26,24 +26,6 @@ public class OrderApi {
     private TransactionBl transactionBl;
     private OrderBl orderBl;
 
-    @Autowired
-    public OrderApi(TransactionBl transactionBl, OrderBl orderBl){
-        this.orderBl = orderBl;
-        this.transactionBl = transactionBl;
-    }
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderApi.class);
-    /*
-     * Este endpoint no sirve para cambiar el estado de una orden en la base de datos, buscandolo mediante la varible 'orderId'.
-     */
-    @RequestMapping(value = "/state/{orderId}", method = RequestMethod.PATCH, produces = MediaType.APPLICATION_JSON_VALUE, consumes= MediaType.APPLICATION_JSON_VALUE)
-    public Order updateProduct(@RequestBody Order order, OrderState orderState, HttpServletRequest request, @PathVariable int orderId){
-        Transaction transaction = TransactionUtil.createTransaction(request);
-
-        transactionBl.createTransaction(transaction);
-        Order productResponse = orderBl.updateOrderState(order,orderState,transaction, orderId );
-        return productResponse;
-    }
 
     /*
      * Este endpoint no sirve para cambiar la dirrecion de entrega de una orden en la base de datos, buscandolo mediante la varible 'orderId'.
